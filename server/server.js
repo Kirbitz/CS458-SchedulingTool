8921d27584ea2daab2d7b7fdd689749db404929a
Kirbitz <tyler.marefke@gmail.com>
2022-10-09 17:43:30 -0500

    Implement env config for server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const app = require('./index.js')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
