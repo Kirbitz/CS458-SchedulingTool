56d20b6a98497b61cc56f5eed924f2ce598351ca
Kirbitz <tyler.marefke@gmail.com>
2022-10-13 17:33:48 -0500

    Fixed Linting issues

--- client/Components/Week.jsx
+++ client/Components/Week.jsx
@@ -1,6 +1,5 @@
 const React = require('react')
 
-export default function Week(props){
-  return (<div>week</div>);
+export default function Week (props) {
+  return (<div>week</div>)
 }
-
--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,6 @@
+import Week from './Components/Week.jsx'
 const React = require('react')
 const { createRoot } = require('react-dom/client')
-import Week from './Components/Week.jsx'
 
 // Start point for the entire front end of the web app
 createRoot(
