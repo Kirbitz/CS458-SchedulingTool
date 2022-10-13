0d3fb884088a9815945a1c7190c9d0e7e54c9705
Kirbitz <tyler.marefke@gmail.com>
2022-10-13 13:46:07 -0500

    updated file?

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,7 @@
 const myApp = require('../server/index.js')
 const request = require('supertest')
-describe("app testing Base Address Tests", () => {
-  it("Connecting to the base address", async () => {
+describe('app testing Base Address Tests', () => {
+  it('Connecting to the base address', async () => {
     const response = await request(myApp).get('/')
     expect(response.statusCode).toBe(200)
   })
