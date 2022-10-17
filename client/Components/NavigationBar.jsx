bc6b443540bbe050d0a019e037d91f3f9149a6a5
Kirbitz <tyler.marefke@gmail.com>
2022-10-16 20:48:00 -0500

    ST-19 Updated var name and extended navbar

--- client/Components/NavigationBar.jsx
+++ client/Components/NavigationBar.jsx
@@ -7,14 +7,14 @@ import { AppBar, Box, Container, IconButton, Toolbar, Tooltip } from '@mui/mater
 import { Business, CalendarMonth, Dashboard, Person, Portrait, Settings } from '@mui/icons-material'
 
 export default function NavigationBar (props) {
-  const { current } = props
+  const { currentlySelected } = props
 
   const selectedColor = 'warning'
   const nonSelectedColor = 'primary'
 
   return (
     <AppBar data-testid="navigation-bar">
-      <Container maxWidth="xl">
+      <Container maxWidth={false}>
         <Toolbar disableGutters>
           <img
             src='\images\UWStoutLogo.png'
@@ -23,33 +23,33 @@ export default function NavigationBar (props) {
             width='30px'
           />
           <Tooltip title="Dashboard" data-testid="Dashboard">
-              <IconButton component={Link} to="/dashboard" size="large" color={current === 'Dashboard' ? selectedColor : nonSelectedColor} sx={{ mx: 1 }}>
+              <IconButton component={Link} to="/dashboard" size="large" color={currentlySelected === 'Dashboard' ? selectedColor : nonSelectedColor} sx={{ mx: 1 }}>
                 <Dashboard />
               </IconButton>
           </Tooltip>
           <Tooltip title="Master Schedule" data-testid="Master">
-            <IconButton component={Link} to="/master-schedule" size="large" color={current === 'Master' ? selectedColor : nonSelectedColor } sx={{ mx: 1 }}>
+            <IconButton component={Link} to="/master-schedule" size="large" color={currentlySelected === 'Master' ? selectedColor : nonSelectedColor } sx={{ mx: 1 }}>
               <CalendarMonth />
             </IconButton>
           </Tooltip>
           <Tooltip title="Employee Schedule" data-testid="Employee">
-            <IconButton component={Link} to="/employee-schedule" size="large" color={current === 'Employee' ? selectedColor : nonSelectedColor } sx={{ mx: 1 }}>
+            <IconButton component={Link} to="/employee-schedule" size="large" color={currentlySelected === 'Employee' ? selectedColor : nonSelectedColor } sx={{ mx: 1 }}>
               <Portrait />
             </IconButton>
           </Tooltip>
           <Tooltip title="Staff" data-testid="Staff">
-            <IconButton component={Link} to="/staff" size="large" color={current === 'Staff' ? selectedColor : nonSelectedColor} sx={{ mx: 1 }}>
+            <IconButton component={Link} to="/staff" size="large" color={currentlySelected === 'Staff' ? selectedColor : nonSelectedColor} sx={{ mx: 1 }}>
               <Person />
             </IconButton>
           </Tooltip>
           <Tooltip title="Department" data-testid="Department">
-            <IconButton component={Link} to="/department" size="large" color={current === 'Department' ? selectedColor : nonSelectedColor} sx={{ mx: 1 }}>
+            <IconButton component={Link} to="/department" size="large" color={currentlySelected === 'Department' ? selectedColor : nonSelectedColor} sx={{ mx: 1 }}>
               <Business />
             </IconButton>
           </Tooltip>
           <Box sx={{ flexGrow: 1 }} />
           <Tooltip title="Settings" data-testid="Settings">
-            <IconButton component={Link} to="/settings" size="large" color={current === 'Settings' ? selectedColor : nonSelectedColor } sx={{ mx: 1 }}>
+            <IconButton component={Link} to="/settings" size="large" color={currentlySelected === 'Settings' ? selectedColor : nonSelectedColor } sx={{ mx: 1 }}>
               <Settings />
             </IconButton>
           </Tooltip>
@@ -60,9 +60,9 @@ export default function NavigationBar (props) {
 }
 
 NavigationBar.propTypes = {
-  current: PropTypes.string
+  currentlySelected: PropTypes.string
 }
 
 NavigationBar.defaultProps = {
-  current: ''
+  currentlySelected: ''
 }
--- client/Components/PageRoutes.jsx
+++ client/Components/PageRoutes.jsx
@@ -8,12 +8,12 @@ export default function PageRoutes (props) {
   return (
     <Routes>
       <Route path="/" element={<Navigate to='/login' replace />} />
-      <Route path="/dashboard" element={<NavigationBar current="Dashboard" />} />
-      <Route path="/master-schedule" element={<NavigationBar current="Master" />} />
-      <Route path="/employee-schedule" element={<NavigationBar current="Employee" />} />
-      <Route path="/staff" element={<NavigationBar current="Staff" />} />
-      <Route path="/department" element={<NavigationBar current="Department" />} />
-      <Route path="/settings" element={<NavigationBar current="Settings" />} />
+      <Route path="/dashboard" element={<NavigationBar currentlySelected="Dashboard" />} />
+      <Route path="/master-schedule" element={<NavigationBar currentlySelected="Master" />} />
+      <Route path="/employee-schedule" element={<NavigationBar currentlySelected="Employee" />} />
+      <Route path="/staff" element={<NavigationBar currentlySelected="Staff" />} />
+      <Route path="/department" element={<NavigationBar currentlySelected="Department" />} />
+      <Route path="/settings" element={<NavigationBar currentlySelected="Settings" />} />
       <Route path="/login" element={<NavigationBar />} />
       <Route path="/*" element={<Navigate to="/" replace />} />
     </Routes>
