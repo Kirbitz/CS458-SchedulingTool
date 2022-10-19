7d90d016cbe6223ef22a8cf863b9d51ab735ad1b
Kirbitz <tyler.marefke@gmail.com>
2022-10-18 22:33:48 -0500

    ST-23 formatting pages are fun :)

--- client/Pages/DepartmentPage.jsx
+++ client/Pages/DepartmentPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 
-import { Box, CircularProgress, Typography } from '@mui/material'
+import { Box, Button, CircularProgress, FormControl, IconButton, InputLabel, Select, TextField, Typography } from '@mui/material'
+import { ArrowForward, ArrowBack, Create, PersonSearch } from '@mui/icons-material'
 
 import { getDepartmentStaff } from '../dataHelper.js'
 import NavigationBar from '../Components/NavigationBar.jsx'
@@ -35,9 +36,54 @@ export default function DepartmentPage (props) {
   return (
     <React.Fragment>
       <NavigationBar selected="Department" />
-      <Typography variant="h2" component="h2">
-        Department
+      <Typography variant="h3" component="h2" align='center' sx={{ mt: 2 }}>
+        Department: The amazing department
       </Typography>
+      <Box display="flex" justifyContent="space-between" sx={{ mt: 2 }}>
+        <Button variant="contained" color='success'>Save</Button>
+        <Box flexGrow={1} />
+        <TextField id="employee-search" label="Employee Id/Name" variant="outlined" />
+        <Button variant="contained" sx={{ mx: 1 }}>
+          <PersonSearch />
+        </Button>
+        <Button variant="contained">
+          <Create />
+        </Button>
+      </Box>
+      <Box display='flex' justifyContent='space-between' sx={{ mt: 4 }}>
+        <FormControl sx={{ width: 450 }}>
+          <InputLabel shrink htmlFor="in-department">In Department</InputLabel>
+          <Select
+            multiple
+            native
+            height='100%'
+            label="In Department"
+            inputProps={{
+              id: 'in-department'
+            }}
+          ></Select>
+        </FormControl>
+        <Box display='flex' sx={{ flexDirection: 'column' }}>
+          <IconButton>
+            <ArrowForward />
+          </IconButton>
+          <br />
+          <IconButton>
+            <ArrowBack />
+          </IconButton>
+        </Box>
+        <FormControl sx={{ width: 450 }}>
+          <InputLabel shrink htmlFor="not-in-department">Not In Department</InputLabel>
+          <Select
+            multiple
+            native
+            label="Not In Department"
+            inputProps={{
+              id: 'not-in-department'
+            }}
+          ></Select>
+        </FormControl>
+      </Box>
     </React.Fragment>
   )
 }
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use((req, response, next) => {
 app.use(Express.static('public'))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join('public', 'index.html'))
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
 })
 
 module.exports = app
