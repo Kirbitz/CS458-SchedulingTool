53498c764b10ebd671133405e584939cc7e1ff71
Garrett Preston <19gprest@gmail.com>
2022-10-20 14:26:45 -0500

    Updating to figure out database problem

--- server/TestQueries.js
+++ server/TestQueries.js
@@ -4,7 +4,9 @@ const knex = require('knex')(dbConfig)
 const router = new Express.Router()
 
 router.get('/studentList', (req, res) => {
-  res.send(knex.select('USER(), CURRENT_USER()')
+  res.send(knex.select('userName')
+    .from('User')
+    .where('userId == 1')
     .then(result => {
       return result
     }))
