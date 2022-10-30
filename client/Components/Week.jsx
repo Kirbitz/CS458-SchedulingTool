c27ce1d0e9a0fb1393bae121411f29bce31ad3ea
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2022-10-30 17:29:53 -0500

    Added WeekChange to MasterView
    Added and implemented a Component called "WeekChange" to the weekly schedule master view.
    Still need to add Hooks to it.
    Should be able to add more buttons to it to increase functionality.
    Also cleaned up indentation of Week.jsx slightly.

--- client/Components/Week.jsx
+++ client/Components/Week.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 import Day from './Day.jsx'
 import Timeline from './Timeline.jsx'
-// import { Grid } from '@mui/material'
+
+// import { Button, Toolbar } from '@mui/material'
 
 // const dateData = [{ weekday: 'Monday', date: 23 }]
 
 // Week component for the master schedule view
 export default function Week (props) {
   return (
-
-                <div style={{ position: 'absolute', left: '10px', display: 'flex', flexDirection: 'row' }}>
-                  <Timeline/>
-                  <Day weekday={ 'Monday' }/>
-                  <Day weekday={ 'Tuesday' }/>
-                  <Day weekday={ 'Wednesday' }/>
-                  <Day weekday={ 'Thursday' }/>
-                  <Day weekday={ 'Friday' }/>
-                  <Day weekday={ 'Saturday' }/>
-                  <Day weekday={ 'Sunday' }/>
-                </div>
+    <div style={{ position: 'absolute', left: '10px', display: 'flex', flexDirection: 'row' }}>
+      <Timeline/>
+      <Day weekday={ 'Monday' }/>
+      <Day weekday={ 'Tuesday' }/>
+      <Day weekday={ 'Wednesday' }/>
+      <Day weekday={ 'Thursday' }/>
+      <Day weekday={ 'Friday' }/>
+      <Day weekday={ 'Saturday' }/>
+      <Day weekday={ 'Sunday' }/>
+    </div>
   )
 }
