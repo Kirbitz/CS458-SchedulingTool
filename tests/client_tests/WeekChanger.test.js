ff97f0f3c2989994bb05b47b1795035b1d66d613
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2022-11-09 21:41:09 -0600

    Update WeekChanger.test.js
    Implemented the following tests for WeekChanger:
    * Initial Render
    * Previous Button Test
    * Next Button Test
    
    The button tests suggest that WeekChanger could be refactored to test that today's date (called week in WeekChanger.jsx) itself is being changed. For now, they test that the text shows the correct dates.
    100% code coverage.

--- tests/client_tests/WeekChanger.test.js
+++ tests/client_tests/WeekChanger.test.js
@@ -1,30 +1,48 @@
-import WeekChanger, { goBackOneWeek, goForwardOneWeek } from '../../client/Components/WeekChanger.jsx'
+import WeekChanger from '../../client/Components/WeekChanger.jsx'
+// import WeekChanger, { goBackOneWeek, goForwardOneWeek } from '../../client/Components/WeekChanger.jsx'
+
 import React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 
 describe('Tests for <WeekChanger />', () => {
-  it('<WeekChanger /> Initial Render', () => {
+  it('Initial Render', () => {
     const component = render(<WeekChanger />)
+    const mondayDate = new Date()
+    const sundayNextDate = new Date()
+
+    mondayDate.setDate((mondayDate.getDate() - mondayDate.getDay()) + 1)
+    sundayNextDate.setDate(sundayNextDate.getDate() + (7 - sundayNextDate.getDay()))
 
     expect(document.body).toBe(component.baseElement)
-    expect(component.baseElement.outerHTML).toContain(' thru ')
+    expect(component.baseElement.outerHTML).toContain(
+      (mondayDate.getMonth() + 1) + '/' + (mondayDate.getDate()) + ' thru ' + (sundayNextDate.getMonth() + 1) + '/' + (sundayNextDate.getDate())
+    )
   })
 
-  it('<WeekChanger /> goBackOneWeek() Function Test', () => {
-    jest.mock('../../client/Components/WeekChanger.jsx', () => ({ goBackOneWeek: jest.fn() }))
-    const previousWeekDate = new Date()
-    previousWeekDate.setDate(previousWeekDate.getDate() - 7)
+  it('Previous Week Button Test', async () => {
+    const component = render(<WeekChanger />)
+    const mondayDate = new Date()
+    const sundayNextDate = new Date()
+    mondayDate.setDate(((mondayDate.getDate() - mondayDate.getDay()) + 1) - 7)
+    sundayNextDate.setDate((sundayNextDate.getDate() + (7 - sundayNextDate.getDay())) - 7)
 
-    expect(goBackOneWeek(new Date())).toBe(previousWeekDate)
+    await fireEvent.click(component.getByTestId('previous-week-button'))
+
+    expect(component.baseElement.outerHTML).toContain(
+      (mondayDate.getMonth() + 1) + '/' + (mondayDate.getDate()) + ' thru ' + (sundayNextDate.getMonth() + 1) + '/' + (sundayNextDate.getDate())
+    )
   })
+  it('Next Button Test', async () => {
+    const component = render(<WeekChanger />)
+    const mondayDate = new Date()
+    const sundayNextDate = new Date()
+    mondayDate.setDate(((mondayDate.getDate() - mondayDate.getDay()) + 1) + 7)
+    sundayNextDate.setDate((sundayNextDate.getDate() + (7 - sundayNextDate.getDay())) + 7)
 
-  it('<WeekChanger /> goForwardOneWeek() Function Test', () => {
-    jest.mock('../../client/Components/WeekChanger.jsx', () => ({ goForwardOneWeek: jest.fn() }))
-    const nextWeekDate = new Date()
-    nextWeekDate.setDate(nextWeekDate.getDate() + 7)
+    await fireEvent.click(component.getByTestId('next-week-button'))
 
-    expect(goForwardOneWeek(new Date())).toBe(nextWeekDate)
+    expect(component.baseElement.outerHTML).toContain(
+      (mondayDate.getMonth() + 1) + '/' + (mondayDate.getDate()) + ' thru ' + (sundayNextDate.getMonth() + 1) + '/' + (sundayNextDate.getDate())
+    )
   })
-  // TODO: Add test for Previous Week button
-  // TODO: Add test for Next Week button
 })
