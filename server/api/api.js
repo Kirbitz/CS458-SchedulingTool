bcf4f5da01b9e7270c27c2c9042b2945d99885fc
Kirbitz <tyler.marefke@gmail.com>
2022-11-11 13:44:15 -0600

    Rebuilding

--- server/api/api.js
+++ server/api/api.js
@@ -8,13 +8,8 @@ router.use(Express.json())
 router.use(RateLimiters.loginLimiter)
 
 router.get('/test', (req, res) => {
-  if (process.env.PORT === '3000') {
-    res.status(200).json({
-      message: 'hello world'
-    })
-  }
-  res.status(404).json({
-    message: 'not found'
+  res.status(200).json({
+    message: 'hello world'
   })
 })
 
