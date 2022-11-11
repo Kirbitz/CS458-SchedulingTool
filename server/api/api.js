aeac319a68e293a9eb732bc9b58924b8122e3461
Kirbitz <tyler.marefke@gmail.com>
2022-11-11 13:32:23 -0600

    Checking envs

--- server/api/api.js
+++ server/api/api.js
@@ -8,8 +8,13 @@ router.use(Express.json())
 router.use(RateLimiters.loginLimiter)
 
 router.get('/test', (req, res) => {
-  res.json({
-    message: 'hello world'
+  if (process.env.PORT === '3000') {
+    res.status(200).json({
+      message: 'hello world'
+    })
+  }
+  res.status(404).json({
+    message: 'not found'
   })
 })
 
