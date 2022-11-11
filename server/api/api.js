035564de0e92595bbe0832e42060fcbc1f1bd5d7
Kirbitz <tyler.marefke@gmail.com>
2022-11-11 14:10:32 -0600

    Updated workflows and removed testing code

--- server/api/api.js
+++ server/api/api.js
@@ -7,12 +7,6 @@ router.use(Express.urlencoded({ extended: true }))
 router.use(Express.json())
 router.use(RateLimiters.loginLimiter)
 
-router.get('/test', (req, res) => {
-  res.status(200).json({
-    message: 'hello world'
-  })
-})
-
 router.post('/login', loginCallback)
 
 module.exports = router
