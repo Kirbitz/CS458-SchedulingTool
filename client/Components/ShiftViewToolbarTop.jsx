ae81114d2d66223d3b7be98ae3a2153cded36f9e
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2022-11-16 18:29:24 -0600

    Add elements to ShiftViewToolbarTop
    Added MUI elements to ShiftView's top toolbar.
    Add Shift button will redirect to the Time Block editor. Since that page does not exist yet, the button is disabled.
    Filter button will contain a list of filter options for all shifts on that day. Filter button's primary color matches the navigation bar's background color. This button will be enabled once that functionality is implemented. It may be removed entirely if it cannot be completed before the end of CS-458.
    Close button currently navigates back to /master-schedule. There are plans to add a confirmation modal to the page if changes are made.

--- client/Components/ShiftViewToolbarTop.jsx
+++ client/Components/ShiftViewToolbarTop.jsx
@@ -1,3 +1,64 @@
-// import React from 'react'
+import React from 'react'
+// import PropTypes from 'prop-types'
 
-// import {} from '@mui/material' // MUI Objects like Box, Grid, Toolbar
+import { Button, Grid, IconButton, Stack } from '@mui/material'
+import { Add, Close, FilterAlt } from '@mui/icons-material'
+import { useNavigate } from 'react-router-dom'
+
+// TODO: Create Time Block Editor before enabling Add Shift button.
+// TODO: Create Filter list before enabling Filter button.
+
+export default function ShiftViewToolbarTop (props) {
+  const navigate = useNavigate()
+  // TODO: Validate date prop
+  // const { date } = props
+  return (
+    <Grid container>
+      <Grid item xs={ 4 }>
+        <Stack
+          spacing={2}
+          direction="row"
+          alignItems="center"
+          justifyContent="flex-start"
+        >
+          <Button variant="contained" endIcon={<Add />} disabled>
+            Add Shift
+          </Button>
+          {
+          // TODO: Add date below after prop validation
+          }
+          <p>MM/DD/YYYY</p>
+        </Stack>
+      </Grid>
+      <Grid item xs={ 4 }>
+        <Stack
+          spacing={2}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+        >
+            <Button sx={{ bgcolor: '#0b233f' }} variant="contained" endIcon={<FilterAlt />} disabled>
+              Filter
+            </Button>
+        </Stack>
+      </Grid>
+      <Grid item xs={ 4 }>
+        <Stack
+          spacing={2}
+          direction="row"
+          alignItems="center"
+          justifyContent="flex-end"
+        >
+          <IconButton
+            aria-label="close"
+            onClick={() => {
+              navigate('/master-schedule')
+            }}
+          >
+            <Close />
+          </IconButton>
+        </Stack>
+      </Grid>
+    </Grid>
+  )
+}
--- client/Pages/ShiftView.jsx
+++ client/Pages/ShiftView.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
 
 import NavigationBar from '../Components/NavigationBar.jsx'
-// import ShiftToolbarBottom from '../Components/ShiftToolbarBottom.jsx'
-// import ShiftToolbarTop from '../Components/ShiftToolbarTop.jsx'
+// import ShiftViewToolbarBottom from '../Components/ShiftViewToolbarBottom.jsx'
+import ShiftViewToolbarTop from '../Components/ShiftViewToolbarTop.jsx'
 // import ShiftViewBody from '../Components/ShiftViewBody.jsx'
 
 import { Box } from '@mui/material'
 
-export default function ShiftView (props) {
+// Shift View page which allows a manager to assign employees to certain shifts
+export default function ShiftView (props) { // props.date
   return (
-    <Box>
-      <NavigationBar />
-      <h1>You did it!</h1>
-    </Box>
+    <div
+      style={{
+        position: 'absolute',
+        left: '0px',
+        right: '0px'
+      }}
+    >
+      <Box>
+        <NavigationBar />
+        <ShiftViewToolbarTop date='DATE WILL GO HERE'/>
+      </Box>
+    </div>
   )
 }
+/** Notes for Shift View date retrieval:
+ ** Date is passed on line 15.
+ ** UI design PPT is in #diagrams-frontend.
+ * TODO: Assemble ShiftViewToolbarTop.
+ * TODO: Assemble ShiftViewToolbarBottom.
+ * TODO: Assemble ShiftViewBody.
+ * TODO: Assemble Shift.
+ * TODO: Find some way to pass the date to this component depending on which day button is pressed.
+ * ? Should the date be put in the Master View page or in the Day component?
+ * ? Pass the date from Day to Master View using callback, pass date to ShiftView, then navigate?
+*/
