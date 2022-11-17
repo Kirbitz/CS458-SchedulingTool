262764e2637cd616e804a5e09e644f94e94119a4
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2022-11-16 20:38:51 -0600

    Added framework to ShiftViewBody.jsx
    Added some basic React elements to Shift View's body for ease of implementation.
    This component is thoroughly unfinished, as the comments indicate.

---
+++ client/Components/ShiftViewBody.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import Shift from '../Components/Shift.jsx'
+
+/** Notes for Shift View Body:
+ * TODO: Validate date prop.
+ * TODO: Build literally everything else.
+ */
+
+/** Body of Shift View which contains a scrollable list of all shifts.
+* Shifts have additional functions which allow shift editing, shift
+* assignment, shift unassignment, and shift deletion.
+*/
+export default function ShiftViewBody (props) {
+  // const { date } = props
+  return (
+    <div>
+    </div>
+  )
+}
