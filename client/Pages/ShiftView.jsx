cda19e00cfe20c0601f40e709050433513050ef8
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2022-11-16 20:40:22 -0600

    ShiftView page formatting + comment reformat
    Updated formatting for Shift View page.
    Having trouble getting everything aligned the way it needs to be.
    Updated comments to a more readable standard.

--- client/Pages/ShiftView.jsx
+++ client/Pages/ShiftView.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 import NavigationBar from '../Components/NavigationBar.jsx'
-// import ShiftViewToolbarBottom from '../Components/ShiftViewToolbarBottom.jsx'
+import ShiftViewToolbarBottom from '../Components/ShiftViewToolbarBottom.jsx'
 import ShiftViewToolbarTop from '../Components/ShiftViewToolbarTop.jsx'
-// import ShiftViewBody from '../Components/ShiftViewBody.jsx'
+import ShiftViewBody from '../Components/ShiftViewBody.jsx'
 
-import { Box } from '@mui/material'
+import { Box, Grid } from '@mui/material'
+
+/** Notes for Shift View date retrieval:
+ ** Date is passed on line 15.
+ ** UI design PPT is in #diagrams-frontend.
+ * TODO: Assemble ShiftViewBody.
+ * TODO: Assemble Shift.
+ * TODO: Find some way to pass the date to this component depending on which day button is pressed.
+ * TODO: Change formatting so bottom toolbar is actually at the bottom of the page.
+ * ? Should the date be put in the Master View page or in the Day component?
+ * ? Pass the date from Day to Master View using callback, pass date to ShiftView, then navigate?
+*/
 
 // Shift View page which allows a manager to assign employees to certain shifts
 export default function ShiftView (props) { // props.date
@@ -17,21 +28,22 @@ export default function ShiftView (props) { // props.date
         right: '0px'
       }}
     >
-      <Box>
+      <Box sx={{
+        flexDirection: 'column'
+      }}>
         <NavigationBar />
-        <ShiftViewToolbarTop date='DATE WILL GO HERE'/>
+        <Grid container direction="column" justifyContent="space-between">
+          <Grid item xs={ 2 }>
+            <ShiftViewToolbarTop date='DATE WILL GO HERE'/>
+          </Grid>
+          <Grid item xs>
+            <ShiftViewBody date='DATE WILL GO HERE'/>
+          </Grid>
+          <Grid item xs={ 2 }>
+            <ShiftViewToolbarBottom />
+          </Grid>
+        </Grid>
       </Box>
     </div>
   )
 }
-/** Notes for Shift View date retrieval:
- ** Date is passed on line 15.
- ** UI design PPT is in #diagrams-frontend.
- * TODO: Assemble ShiftViewToolbarTop.
- * TODO: Assemble ShiftViewToolbarBottom.
- * TODO: Assemble ShiftViewBody.
- * TODO: Assemble Shift.
- * TODO: Find some way to pass the date to this component depending on which day button is pressed.
- * ? Should the date be put in the Master View page or in the Day component?
- * ? Pass the date from Day to Master View using callback, pass date to ShiftView, then navigate?
-*/
