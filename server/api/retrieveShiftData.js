d0abaf4bde507cc88ef993cfb957b80b5ecd3167
CodePerson <35588954+comessErinaceus@users.noreply.github.com>
2022-11-17 01:33:39 -0600

    Update retrieveShiftData.js
    retrieveShiftData now queries the DB for specific days as json.

--- server/api/retrieveShiftData.js
+++ server/api/retrieveShiftData.js
@@ -1,20 +1,29 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const { verifyJWTAuthToken } = require('./dataHelper')
+// *****commented out so we don't need to worry about security for now*****
+// const { verifyJWTAuthToken } = require('./dataHelper')
+// *****commented out so we don't need to worry about security for now*****
 
 // Database connection with knex
 const dbClient = require('./dbClient')
 
-// selecting data from the database
+// retrieve the set of timeBlocks for a specific day
 const retrieveShiftData = async (req, res) => {
+  // ***these vars are hardcoded but will come from the front end date picker later***
+  const dateFrom = '2022-11-17 00:00:00'
+  const dateTo = '2022-11-18 00:00:00'
+
   console.log('retrieveShift Called')
+  // *****commented out so we don't need to worry about security for now*****
   // try {
   //   verifyJWTAuthToken(req, res)
   // } catch (err) {
   // return dbClient.select('*').from(+'TimeBlock')
   // }
-  const testingConnect = await dbClient.select('userId').from('User').then(result => { return result })
+  // *****commented out so we don't need to worry about security for now*****
+  const testingConnect = await dbClient.select('timeId', 'timeStart', 'timeEnd', 'timeType', 'positionId', 'deptId')
+    .from('TimeBlock').where('timeStart', '>=', dateFrom).where('timeEnd', '<', dateTo).then(result => { return result })
   console.log(testingConnect)
   res.send(testingConnect)
 
