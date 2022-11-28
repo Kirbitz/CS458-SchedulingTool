611786bd985ca91b41714327a4cd3fdd56591049
Blackkirby72 <47089182+Blackkirby72@users.noreply.github.com>
2022-11-28 17:56:25 -0600

    wrote tests for /api/day

--- server/api/retrieveShiftData.js
+++ server/api/retrieveShiftData.js
@@ -1,9 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-// *****commented out so we don't need to worry about security for now*****
-// const { verifyJWTAuthToken } = require('./dataHelper')
-// *****commented out so we don't need to worry about security for now*****
+const { verifyJWTAuthToken } = require('./dataHelper')
 
 // Database connection with knex
 const dbClient = require('./dbClient')
@@ -11,17 +9,17 @@ const dbClient = require('./dbClient')
 // retrieve the set of timeBlocks for a specific day
 const retrieveShiftData = async (req, res) => {
   // ***these vars are hardcoded but will come from the front end date picker later***
-  const dateFrom = '2022-11-14 00:00:00'
-  const dateTo = '2022-11-20 00:00:00'
+  const dateFrom = req.body.startDate
+  const dateTo = req.body.endDate
 
-  console.log('retrieveShift Called')
-  // *****commented out so we don't need to worry about security for now*****
-  // try {
-  //   verifyJWTAuthToken(req, res)
-  // } catch (err) {
-  // return dbClient.select('*').from(+'TimeBlock')
-  // }
-  // *****commented out so we don't need to worry about security for now*****
+  // const dateFrom = '2022-11-14 00:00:00'
+  // const dateTo = '2022-11-20 00:00:00'
+  try {
+    verifyJWTAuthToken(req, res)
+  } catch (err) {
+    return
+  }
+
   // ** consider user id which is set in verify auth
   const testingConnect = await dbClient.select('timeId',
     'timeStart',
@@ -37,14 +35,13 @@ const retrieveShiftData = async (req, res) => {
     .where('timeStart', '>=', dateFrom)
     .where('timeEnd', '<', dateTo)
     .then(result => { return result })
-  // console.log(testingConnect)
+
   const days = new Map()
   testingConnect.forEach((value) => {
     // console.log(key, value)
     // parse the date out of the time start part of the time block
     // console.log(value.timeStart)
     // only insert dates that do not already exist in the map
-    console.log('the value we want to map: ' + value + '/n')
     if (!days.get(value.timeStart)) {
       // create the object that we want to put into the map
       // console.log(value)
@@ -53,9 +50,8 @@ const retrieveShiftData = async (req, res) => {
     // example of getting data out of the map
     // console.log(' Days in map: ' + days.get(value.timeStart).positionName)
   })
-  // console.log(' Days in map: ' + days.get(dateFrom).positionName)
-  console.log(days)
-  res.send(testingConnect)
+
+  res.status(200).send(testingConnect)
 
 // const user = await module.exports.
 }
--- tests/server_tests/retrieveShiftData.test.js
+++ tests/server_tests/retrieveShiftData.test.js
@@ -1,5 +1,55 @@
-// TODO
-// test db connection
-// test sql query returns correct data
-// test callback
 // error proof the server code
+
+const myApp = require('../../server/index')
+const supertest = require('supertest')
+const request = supertest(myApp)
+const dataHelper = require('../../server/api/dataHelper')
+
+jest.mock('../../server/api/dbClient', () => ({
+  where: jest.fn().mockReturnThis(),
+  select: jest.fn().mockReturnThis(),
+  from: jest.fn().mockReturnThis(),
+  join: jest.fn().mockReturnThis(),
+  then: jest.fn().mockReturnValue([{ timeId: 1 }])
+}))
+
+jest.mock('knex')
+jest.mock('../../server/api/dataHelper')
+
+describe('Testing retrieveShiftData.js', () => {
+  beforeAll(() => {
+    jest.spyOn(dataHelper, 'verifyJWTAuthToken').mockImplementation(jest.fn(() => { }))
+  })
+
+  it('Successful retrieval of days', async () => {
+    const response = await request.get('/api/day')
+      .send({
+        startDate: '2022-11-14 00:00:00',
+        endDate: '2022-11-20 00:00:00'
+      })
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  it('failed auth', async () => {
+    jest.spyOn(dataHelper, 'verifyJWTAuthToken').mockImplementation((req, res) => {
+      res.status(401)
+        .json({
+          error: {
+            status: 401,
+            message: 'Unauthorized'
+          }
+        })
+        .end()
+      throw new Error('I am an error')
+    })
+
+    const response = await request.get('/api/day')
+      .send({
+        startDate: '2022-11-14 00:00:00',
+        endDate: '2022-11-20 00:00:00'
+      })
+
+    expect(response.statusCode).toBe(401)
+  })
+})
