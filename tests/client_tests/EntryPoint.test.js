fe0c82a3f5d66e415fa18d31ad1b282a9e77cd6a
MrZalgo <98833844+MrZalgo@users.noreply.github.com>
2022-11-27 19:53:43 -0600

    Unused Import removal
    Removed unused import within entrypoint.

--- tests/client_tests/EntryPoint.test.js
+++ tests/client_tests/EntryPoint.test.js
@@ -1,7 +1,6 @@
 import EntryPoint from '../../client/EntryPoint.jsx'
 import React from 'react'
 import { render } from '@testing-library/react'
-import BrowserRouter from 'react-router-dom'
 
 it('Test for <EntryPoint />', () => {
   const component = render(<EntryPoint />)
