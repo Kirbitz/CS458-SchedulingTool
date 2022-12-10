1f78b50c2b6c4d7e90392170d7da9efe13fd483a
Kirbitz <tyler.marefke@gmail.com>
2022-12-09 20:44:41 -0600

    Removing Artifacts

--- tests/client_tests/Router.test.js
+++ tests/client_tests/Router.test.js
@@ -8,7 +8,6 @@ import PageRoutes from '../../client/Components/PageRoutes.jsx'
 // Component for checking the route is correct
 const LocationDisplay = () => {
   const location = useLocation()
-  console.log(location)
 
   return <div data-testid="location-display">{location.pathname}</div>
 }
